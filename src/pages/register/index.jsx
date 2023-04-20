9226a1ace9447e5b3dc1a1da7df5204fe51a5e44
Martin Mroz <mamr@hotmail.no>
2023-04-20 18:55:00 +0200

    styled form

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { baseURL } from "../../utilities/constants";
-import Alert from "@mui/material/Alert";
+import { FormContainer } from "./styled";
+
+import Alert from '@mui/material/Alert';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 const RegisterCustomerForm = () => {
   const [name, setName] = useState("");
@@ -10,8 +14,6 @@ const RegisterCustomerForm = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [avatar, setAvatar] = useState("");
   const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
-  const [warning, setWarning] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -20,65 +22,53 @@ const RegisterCustomerForm = () => {
       return;
     }
     try {
-      const response = await axios.post(baseURL + "auth/register", {
-        name,
-        email,
-        password,
-        avatar,
-        venueManager: false,
-      });
+      const response = await axios.post(
+        baseURL + "auth/register",
+        {
+          name,
+          email,
+          password,
+          avatar,
+          venueManager: false,
+        }
+      );
       console.log(response.data);
-      setSuccess("Registration successful!");
+      setError("Registration successful!");
     } catch (error) {
-      setError(error.response.data.message);
-      setWarning(`Registration failed: ${error.response.data.message}`);
+      setError(`Registration failed: ${error.response.data.message}`);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Email:
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Confirm Password:
-        <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Avatar:
-        <input type="url" value={avatar} onChange={(e) => setAvatar(e.target.value)} />
-      </label>
-      <br />
-      {error && (
-        <Alert severity="error" onClose={() => setError("")}>
-          {error}
-        </Alert>
-      )}
-      {success && (
-        <Alert severity="success" onClose={() => setSuccess("")}>
-          {success}
-        </Alert>
-      )}
-      {warning && (
-        <Alert severity="warning" onClose={() => setWarning("")}>
-          {warning}
-        </Alert>
-      )}
-      <button type="submit">Register</button>
-    </form>
+    <FormContainer>
+    
+      {error && <Alert severity="error">{error}</Alert>}
+      <form onSubmit={handleSubmit}>
+        
+    
+          <TextField type="text" label="Name" value={name} onChange={(e) => setName(e.target.value)} style={{ marginBottom: '10px' }} />
+        <br />
+ 
+          <TextField type="email" label="Email" value={email} onChange={(e) => setEmail(e.target.value)} style={{ marginBottom: '10px' }} />
+   
+        <br />
+
+          <TextField type="password" label="Password" value={password} onChange={(e) => setPassword(e.target.value)} style={{ marginBottom: '10px' }} />
+    
+        <br />
+ 
+          <TextField type="password" label="Confirm Password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} style={{ marginBottom: '10px' }} />
+ 
+        <br />
+   
+          <TextField type="url" label="Avatar" value={avatar} onChange={(e) => setAvatar(e.target.value)} style={{ marginBottom: '10px' }} />
+ 
+        <br />
+        <Button variant="contained" type="submit" style={{ marginTop: '10px' }}>Register</Button>
+      </form>
+      {error && <Alert severity="warning">Please fix the errors above and try again.</Alert>}
+      
+      </FormContainer>
   );
 };
 
