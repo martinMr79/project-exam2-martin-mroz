dcbe9f04e5b74cc66ea73755029db076e927438e
Martin Mroz <mamr@hotmail.no>
2023-04-23 23:22:09 +0200

    logout

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,34 +1,44 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { StyledNav } from "./styled";
 
 import MenuIcon from '@mui/icons-material/Menu';
 import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
 
 function Nav() {
   const accessToken = localStorage.getItem("accessToken"); // Get the access token from local storage
   const isLoggedIn = accessToken != null; // Check if the access token is present or not
-  
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    localStorage.removeItem("accessToken"); // Remove the access token from local storage
+    navigate("/login"); // Redirect the user to the login page
+  }
+
   return (
     <StyledNav>
       <div className="left">
-        <a href="/"><div>Holidaze</div></a>
+        <a href="/">
+          <div>Holidaze</div>
+        </a>
       </div>
       <MenuIcon className="menu-icon" />
       <div className="right">
-        {isLoggedIn ? ( // If the user is logged in, render the Avatar component
-          <Avatar alt="User Avatar" src="/static/images/avatar/1.jpg" />
-        ) : ( // Otherwise, render the "Log in" and "Create account" links
+        {isLoggedIn ? (
+          <>
+            <Avatar alt="User Avatar" src="/static/images/avatar/1.jpg" />
+            <Button onClick={handleLogout}>Logout</Button>
+          </>
+        ) : (
           <>
             <a href="/login">Log in</a>
-            <a href="register">Create account</a>
+            <a href="/register">Create account</a>
           </>
         )}
       </div>
     </StyledNav>
   );
 }
-
-
 
 export { Nav };
