1e025cb6490e526590308282076691377f55b44c
Martin Mroz <mamr@hotmail.no>
2023-05-08 22:51:55 +0200

    manager can add venue. manager update broke

--- src/components/profile/venueManager/index.jsx
+++ src/components/profile/venueManager/index.jsx
@@ -7,12 +7,103 @@ import axios from "axios";
 import { baseURL } from "../../../utilities/constants";
 import { useAuthStore } from "../../../hooks/useAuthStore";
 import { ProfileContainer } from "../styled";
+import Grid from "@mui/material/Grid";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
 
 const ManagerProfile = ({ handleLogout }) => {
   const [avatarURL, setAvatarURL] = useState("");
   const { decodedToken, accessToken, setAccessToken, setDecodedToken } = useAuthStore();
   const history = useNavigate();
   const [loading, setLoading] = useState(true);
+
+
+  const [venue, setVenue] = useState({
+    name: "",
+    description: "",
+    media: [""],
+    price: 0,
+    maxGuests: 0,
+    rating: 5,
+    meta: {
+      wifi: false,
+      parking: false,
+      breakfast: false,
+      pets: false
+    },
+    location: {
+      address: "",
+      city: "",
+      zip: "",
+      country: "",
+      continent: "",
+      lat: 0,
+      lng: 0
+    }
+  });
+
+  const handleMediaChange = (index, event) => {
+    const newMedia = [...venue.media];
+    newMedia[index] = event.target.value;
+    setVenue({
+      ...venue,
+      media: newMedia
+    });
+  };
+
+  const addMediaInput = () => {
+    setVenue({
+      ...venue,
+      media: [...venue.media, ""]
+    });
+  };
+
+  const handleVenueChange = (event) => {
+    const { name, value } = event.target;
+  
+    if (name.startsWith("meta.")) {
+      setVenue({
+        ...venue,
+        meta: {
+          ...venue.meta,
+          [name.replace("meta.", "")]: event.target.checked
+        }
+      });
+    } else if (name.startsWith("location.")) {
+      setVenue({
+        ...venue,
+        location: {
+          ...venue.location,
+          [name.replace("location.", "")]: value
+        }
+      });
+    } else {
+      let fieldValue = value;
+      if (name === "price" || name === "maxGuests") {
+        fieldValue = parseFloat(value);
+      }
+      setVenue({
+        ...venue,
+        [name]: fieldValue
+      });
+    }
+  };
+
+  const handleAddVenue = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        `${baseURL}venues`,
+        venue,
+        {
+          headers: { Authorization: `Bearer ${accessToken}` },
+        }
+      );
+      console.log("Venue added:", response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const handleAvatarUpdate = async (event) => {
     event.preventDefault();
@@ -34,56 +125,152 @@ const ManagerProfile = ({ handleLogout }) => {
       console.error(error);
     }
   };
-
   useEffect(() => {
     const fetchAccessToken = async () => {
-     
+      try {
+        const response = await axios.post(baseURL + "auth/refresh-token", {
+          token: decodedToken.refreshToken,
+        });
+  
+        setAccessToken(response.data.accessToken);
+      } catch (error) {
+        console.error(error);
+        history.push("/login");
+      }
   
       setLoading(false);
     };
   
     fetchAccessToken();
-  
   }, [accessToken, decodedToken, history, setAccessToken]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
   return (
-<ProfileContainer>
-      {decodedToken ? (
-        <>
-         {console.log("Manager profile decoded token:", decodedToken)} 
-          <h2>Welcome VenueManager{decodedToken.name}</h2>
-          <p>Email: {decodedToken.email}</p>
-          <Avatar
-            alt={decodedToken.name}
-            src={decodedToken.avatar}
-            sx={{ width: 200, height: 200 }}
-          />
-          <form onSubmit={handleAvatarUpdate}>
-            <TextField
-              type="text"
-              label="New Avatar URL"
-              value={avatarURL}
-              onChange={(e) => setAvatarURL(e.target.value)}
-              style={{ marginTop: "10px", marginBottom: "10px" }}
-            />
-            <br />
-            <Button variant="contained" type="submit">
-              Update Avatar
-            </Button>
-          </form>
-          <p>Venue Manager: {decodedToken.role === "venueManager" ? "Yes" : "No"}</p>
-          <h2>Add Venue</h2>
-          <Button onClick={handleLogout}>Logout</Button>
-        </>
-      ) : (
-        <div>You are not logged in.</div>
-      )}
-    </ProfileContainer>
-  );
-};
 
-export default ManagerProfile;
+      <ProfileContainer>
+        {decodedToken ? (
+          <>
+            {console.log("Manager profile decoded token:", decodedToken)}
+            <h2>Welcome {decodedToken.name}</h2>
+            <p>Email: {decodedToken.email}</p>
+            <Avatar
+              alt={decodedToken.name}
+              src={decodedToken.avatar}
+              sx={{ width: 200, height: 200 }}
+            />
+     
+            <p>Venue Manager: {decodedToken.role === "venueManager" ? "Yes" : "No"}</p>
+            <h2>Add Venue</h2>
+            <form onSubmit={handleAddVenue}>
+  <Grid container spacing={2}>
+    <Grid item xs={12}>
+      <TextField
+        name="name"
+        label="Venue Name"
+        value={venue.name}
+        onChange={handleVenueChange}
+        fullWidth
+      />
+    </Grid>
+    <Grid item xs={12}>
+      <TextField
+        name="description"
+        label="Description"
+        value={venue.description}
+        onChange={handleVenueChange}
+        fullWidth
+      />
+    </Grid>
+    <Grid item xs={12}>
+  {venue.media.map((mediaUrl, index) => (
+    <TextField
+      key={index}
+      name={`media-${index}`}
+      label={`Media URL ${index + 1}`}
+      value={mediaUrl}
+      onChange={(event) => handleMediaChange(index, event)}
+      fullWidth
+    />
+  ))}
+  <Button onClick={addMediaInput}>Add Another Media URL</Button>
+</Grid>
+<Grid item xs={12}>
+  <TextField
+    name="price"
+    label="Price"
+    value={venue.price}
+    onChange={handleVenueChange}
+    fullWidth
+  />
+</Grid>
+<Grid item xs={12}>
+  <TextField
+    name="maxGuests"
+    label="Max Guests"
+    value={venue.maxGuests}
+    onChange={handleVenueChange}
+    fullWidth
+  />
+</Grid>
+    
+    {/* Add other form fields here as needed */}
+    <Grid item xs={12}>
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={venue.meta.wifi}
+            onChange={handleVenueChange}
+            name="meta.wifi"
+          />
+        }
+        label="WiFi"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={venue.meta.parking}
+            onChange={handleVenueChange}
+            name="meta.parking"
+          />
+        }
+        label="Parking"
+      />
+        <FormControlLabel
+        control={
+          <Checkbox
+            checked={venue.meta.breakfast}
+            onChange={handleVenueChange}
+            name="meta.breakfast"
+          />
+        }
+        label="breakfast"
+      />
+        <FormControlLabel
+        control={
+          <Checkbox
+            checked={venue.meta.pets}
+            onChange={handleVenueChange}
+            name="meta.pets"
+          />
+        }
+        label="pets"
+      />
+      {/* Add other checkbox fields here as needed */}
+    </Grid>
+    <Grid item xs={12}>
+      <Button variant="contained" type="submit">
+        Add Venue
+      </Button>
+    </Grid>
+  </Grid>
+</form>
+  
+            <Button onClick={handleLogout}>Logout</Button>
+          </>
+        ) : (
+          <div>You are not logged in.</div>
+        )}
+      </ProfileContainer>
+    );
+  };
+  
+  export default ManagerProfile;
