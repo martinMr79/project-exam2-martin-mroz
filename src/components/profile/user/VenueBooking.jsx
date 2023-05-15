3fd96a8f669c48a6c2ab17b2407cf3c0f87842df
Martin Mroz <mamr@hotmail.no>
2023-05-15 23:52:40 +0200

    testing react-datePicker

--- src/components/profile/user/VenueBooking.jsx
+++ src/components/profile/user/VenueBooking.jsx
@@ -1,18 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import axios from "axios";
 import { useAuthStore } from "../../../hooks/useAuthStore";
 import { baseURL } from "../../../utilities/constants";
 import { ProfileContainer } from "../styled"; 
+import DatePicker from 'react-date-picker';
 
 const VenueBooking = ({ venueId }) => {
  
   const { accessToken } = useAuthStore();
-  const [dateFrom, setDateFrom] = useState("");
-  const [dateTo, setDateTo] = useState("");
+  const [dateFrom, setDateFrom] = useState(new Date());
+  const [dateTo, setDateTo] = useState(new Date());
   const [guests, setGuests] = useState(0);
   const [message, setMessage] = useState("");
+  const [bookedDates, setBookedDates] = useState([]);
+
+  useEffect(() => {
+    const fetchBookedDates = async () => {
+      try {
+        const response = await axios.get(`${baseURL}bookedDates/${venueId}`, {
+          headers: { Authorization: `Bearer ${accessToken}` },
+        });
+        setBookedDates(response.data.map(dateStr => new Date(dateStr)));
+      } catch (error) {
+        console.error('Failed to fetch booked dates:', error);
+      }
+    };
+
+    fetchBookedDates();
+  }, [venueId, accessToken]);
 
   const handleBooking = async (event) => {
     event.preventDefault();
@@ -38,42 +55,50 @@ const VenueBooking = ({ venueId }) => {
 
   return (
     <ProfileContainer>
-      <form onSubmit={handleBooking}>
-        <h2>Book a Venue</h2>
-        {message && <p>{message}</p>}
-        <TextField
-          type="date"
-          label="From"
-          value={dateFrom}
-          onChange={(e) => setDateFrom(e.target.value)}
-          required
-          style={{ marginBottom: "10px" }}
-        />
-        <br />
-        <TextField
-          type="date"
-          label="To"
-          value={dateTo}
-          onChange={(e) => setDateTo(e.target.value)}
-          required
-          style={{ marginBottom: "10px" }}
-        />
-        <br />
-        <TextField
-          type="number"
-          label="Guests"
-          value={guests}
-          onChange={(e) => setGuests(e.target.value)}
-          required
-          style={{ marginBottom: "10px" }}
-        />
-        <br />
-        <Button variant="contained" type="submit">
-          Book
-        </Button>
-      </form>
-      
-    </ProfileContainer>
+    <form onSubmit={handleBooking}>
+      <h2>Book a Venue</h2>
+      {message && <p>{message}</p>}
+      <DatePicker
+        onChange={setDateFrom}
+        value={dateFrom}
+        minDate={new Date()}
+        maxDate={new Date(Date.now() + 31536000000)}
+        tileDisabled={({ date, view }) =>
+          view === 'month' && // Block the date only in month view
+          bookedDates.some(disabledDate =>
+            disabledDate.getTime() === date.getTime()
+          )
+        }
+        required
+      />
+      <DatePicker
+        onChange={setDateTo}
+        value={dateTo}
+        minDate={dateFrom}
+        maxDate={new Date(Date.now() + 31536000000)}
+        tileDisabled={({ date, view }) =>
+          view === 'month' && // Block the date only in month view
+          bookedDates.some(disabledDate =>
+            disabledDate.getTime() === date.getTime()
+          )
+        }
+        required
+      />
+      <br />
+      <TextField
+        type="number"
+        label="Guests"
+        value={guests}
+        onChange={(e) => setGuests(e.target.value)}
+        required
+        style={{ marginBottom: "10px" }}
+      />
+      <br />
+      <Button variant="contained" type="submit">
+        Book
+      </Button>
+    </form>
+  </ProfileContainer>
   );
 };
 
