969751d29191b271c13b2ee9e7ec3f50cba327af
Martin Mroz <mamr@hotmail.no>
2023-05-26 18:02:05 +0200

    bookings viewed in modal

--- src/components/profile/ManagerProfile/VenueBookings.jsx
+++ src/components/profile/ManagerProfile/VenueBookings.jsx
@@ -1,10 +1,23 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { baseURL } from "../../../utilities/constants";
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from "@mui/material/Button";
 
 const VenueBookings = ({ venueId, accessToken }) => {
   const [venue, setVenue] = useState(null);
-  
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
     const fetchVenueBookings = async () => {
@@ -30,19 +43,28 @@ const VenueBookings = ({ venueId, accessToken }) => {
 
   return (
     <div>
-      <h2>Bookings for this venue</h2>
-      {venue && venue.bookings && venue.bookings.length > 0 ? (
-        venue.bookings.map((booking) => (
-          <div key={booking.id}>
-            <p>Booking ID: {booking.id}</p>
-            <p>Date From: {new Date(booking.dateFrom).toLocaleDateString()}</p>
-            <p>Date To: {new Date(booking.dateTo).toLocaleDateString()}</p>
-            <p>Guests: {booking.guests}</p>
-          </div>
-        ))
-      ) : (
-        <p>No bookings for this venue.</p>
-      )}
+      
+      <Button variant="contained" onClick={handleClickOpen}>View Bookings</Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>My Bookings</DialogTitle>
+        <DialogContent>
+          {venue && venue.bookings && venue.bookings.length > 0 ? (
+            venue.bookings.map((booking) => (
+              <div key={booking.id}>
+                <p>Booking ID: {booking.id}</p>
+                <p>Date From: {new Date(booking.dateFrom).toLocaleDateString()}</p>
+                <p>Date To: {new Date(booking.dateTo).toLocaleDateString()}</p>
+                <p>Guests: {booking.guests}</p>
+              </div>
+            ))
+          ) : (
+            <p>No bookings for this venue.</p>
+          )}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
